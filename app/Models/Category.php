fd6559f53fdd67bfe50f69630065244282304095
Johan Setiawan <johanreadyforsuccess@gmail.com>
2026-05-05 23:15:29 +0700

    Pertemuan 5

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function index()
+    {
+        $categories = Category::withCount('events')->get();
+        return view('admin.categories.index', compact('categories'));
+    }
 }
